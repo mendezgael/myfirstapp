07f2eba0b3d781fc7c769611895380fda1bd52fc
jud1o <mat_net01@outlook.com>
2026-02-20 15:20:46 -0300

    feat: generación de controllers

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller({})
+
+
+    export class TaskController {
+
+        @Get('/tasks')
+        getAllTasks() {
+            return 'Obteniendo todas las tareas';
+        }
+
+    }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TaskController } from './tasks.controller';
 
-@Module({})
-export class TaskModule { }
+
+    @Module({controllers: [TaskController]})
+    export class TaskModule { }
