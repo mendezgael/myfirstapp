35dc8f13160101774d5c6cbd666412604fe11750
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-21 16:33:44 -0300

    feat: Validación en main usando whipelist

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
-export interface CreateTaskDto {
+import {IsString, MinLength} from 'class-validator';
+
+export class  CreateTaskDto {
+    @IsString()
+    @MinLength(1)
     title: string;
+
+    @IsString()
+    @MinLength(1)
     description: string;
 }
