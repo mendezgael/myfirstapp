d7b89f7275e787246a124991b31cb68faae3cc4d
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-23 13:01:28 -0300

    feat: generacion de payments via resources de nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsModule } from './payments/payments.module';
 
 
 @Module({
-  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule],
+  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule, PaymentsModule],
   controllers: [HelloController],
 
 })
