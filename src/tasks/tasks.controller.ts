f84831bd76661542344c0d50a5e73776a733f59f
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-24 17:40:59 -0300

    feat: Aplicación de Swagger para documentación

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,8 +2,10 @@ import { Query, Body, Controller, Delete, Get, Patch, Post, Put, Param } from '@
 import { TasksService } from './tasks.service';
 import type { CreateTaskDto } from './dto/create-task.dto';
 import type { UpdateTaskDto } from './dto/update-task.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('/tasks')
+@ApiTags('tasks')
 
 
     export class TaskController {
@@ -11,17 +13,22 @@ import type { UpdateTaskDto } from './dto/update-task.dto';
         constructor(private tasksService: TasksService) {}
 
         @Get()
+        @ApiOperation({ summary: 'Get all tasks' })
+        @ApiResponse({ status: 200, description: 'Return all tasks.' })
+        @ApiResponse({ status: 403, description: 'Forbidden.' })
         getAllTasks(@Query() query:any) {
             console.log(query);
             return this.tasksService.getTasks();
         }
 
         @Get('/:id')
+        @ApiOperation({ summary: 'Get a task by ID' })
         getTasks(@Param('id') id: string) {
             return this.tasksService.getTask(parseInt(id));
         }
 
         @Post()
+        @ApiOperation({ summary: 'Create a new task' })
         createTasks(@Body() task: CreateTaskDto) {
             return this.tasksService.postTasks(task);
         }
